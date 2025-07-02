fe9db0ebf403e6961feba6f10f2fabe7b95bbefb
FrankPomianoMinaya <frankpomianominaya@gmail.com>
2025-07-02 14:53:13 -0500

    Recibir solo los datos e imprimir en consola
    Se comentó los servicios de conexión a google spreadsheets para enviar los datos a una hoja de cálculo, solo está el codigo operativo para imprimirlo en consola y posteriormente cambiar el destino de los datos procesados.

--- app.py
+++ app.py
@@ -1,46 +1,53 @@
 from flask import Flask, request, jsonify
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import os
-from dotenv import load_dotenv
-load_dotenv()
-credentials = os.getenv("CREDENTIALS")
+#import gspread
+#from oauth2client.service_account import ServiceAccountCredentials
+#import os
+#from dotenv import load_dotenv
+#load_dotenv()
+#credentials = os.getenv("CREDENTIALS")
 
 app = Flask(__name__)
 
 # Definir los alcances (scopes) necesarios
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+#scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
 # Autenticarse usando el archivo de credenciales descargado
-creds = ServiceAccountCredentials.from_json_keyfile_name(
-    credentials, scope)
+#creds = ServiceAccountCredentials.from_json_keyfile_name(
+#    credentials, scope)
 
-client = gspread.authorize(creds)
+#client = gspread.authorize(creds)
 
 # Abrir una hoja de cálculo por su nombre
-sheet = client.open("Prueba de CRM").sheet1
+#sheet = client.open("Prueba de CRM").sheet1
 
-@app.route('/form_submission', methods=['POST'])
+@app.route('/')
+def index():
+    return 'Hello from Flask!'
+
+
+@app.route('/test')
+def test():
+    return 'This is a test route.'
+
+@app.route('/webhook', methods=['POST'])
 def form_submission():
-    datos = request.form
-    print(datos)
     data = request.form  # Recibe los datos del formulario como 'form data'
-    
-    nombre = data.get('name')
-    telefono = data.get('field_9194d5d')
-    email = data.get('email')
-    mensaje = data.get('message')
+    print(data)
+    nombre = data.get('Nombres y Apellidos')
+    telefono = data.get('Teléfono / Celular')
+    email = data.get('Correo Electrónico')
+    mensaje = data.get('Mensaje')
 
     # Guardar los datos en Google Sheets
-    save_to_google_sheets(nombre,telefono, email, mensaje)
+    #save_to_google_sheets(nombre,telefono, email, mensaje)
 
     return jsonify({"message": "Formulario recibido con éxito"}), 200
 
-def save_to_google_sheets(nombre,telefono, email, mensaje):
-    # Guarda los datos en Google Sheets
-    form_data = [nombre,telefono, email, mensaje]
-    sheet.append_row(form_data)
+#def save_to_google_sheets(nombre,telefono, email, mensaje):
+#    # Guarda los datos en Google Sheets
+#    form_data = [nombre,telefono, email, mensaje]
+#    sheet.append_row(form_data)
 
-if __name__ == '_main_':
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
