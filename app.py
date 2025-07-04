fbe69532180704cf0b6dc6a24cc754699f4576c5
Frank Pomiano <thesic19@gmail.com>
2025-07-04 16:40:10 -0500

    Enviar los datos a una db de sql server

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import pyodbc
 #import gspread
 #from oauth2client.service_account import ServiceAccountCredentials
 #import os
@@ -6,7 +9,29 @@ from flask import Flask, request, jsonify
 #load_dotenv()
 #credentials = os.getenv("CREDENTIALS")
 
+# Configura la conexión
+server = r'DESKTOP-44IC9KG\MSSQLSERVER_1'
+database = 'test'
+username = 'user_test'
+password = 'user_777'
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server'
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Modelo
+class Datos(db.Model):
+    __tablename__ = 'Webhook'
+    ID = db.Column(db.Integer, primary_key=True)
+    Nombre = db.Column(db.String(50), nullable=False)
+    Telefono = db.Column(db.String(50), nullable=False)
+    Email = db.Column(db.String(50), nullable=False)
+    Mensaje = db.Column(db.String(50), nullable=False)
 
 # Definir los alcances (scopes) necesarios
 #scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
@@ -37,7 +62,18 @@ def form_submission():
     telefono = data.get('Teléfono / Celular')
     email = data.get('Correo Electrónico')
     mensaje = data.get('Mensaje')
+    print(f"{nombre}\t{telefono}\t{email}\t{mensaje}")
+    if not all([nombre, telefono, email, mensaje]):
+        return jsonify({'error': 'Faltan campos obligatorios'}), 400
 
+    data_db = Datos(
+        Nombre=nombre,
+        Telefono=telefono,
+        Email=email,
+        Mensaje=mensaje
+    )
+    db.session.add(data_db)
+    db.session.commit()
     # Guardar los datos en Google Sheets
     #save_to_google_sheets(nombre,telefono, email, mensaje)
 
--- test.py
+++ test.py
@@ -1,25 +1,24 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import os
-from dotenv import load_dotenv
-load_dotenv()
-credentials = os.getenv("CREDENTIALS")
+#import gspread
+#from oauth2client.service_account import ServiceAccountCredentials
+#import os
+#from dotenv import load_dotenv
+#load_dotenv()
+#credentials = os.getenv("CREDENTIALS")
 # Definir los alcances (scopes) necesarios
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-
+#scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 # Autenticarse usando el archivo de credenciales descargado
-creds = ServiceAccountCredentials.from_json_keyfile_name(
-    credentials, scope)
-
-client = gspread.authorize(creds)
+# creds = ServiceAccountCredentials.from_json_keyfile_name(
+#     credentials, scope)
+import pyodbc
+# client = gspread.authorize(creds)
 
 # Abrir una hoja de cálculo por su nombre
-sheet = client.open("Prueba de CRM").sheet1
+# sheet = client.open("Prueba de CRM").sheet1
 
-def save_to_google_sheets(nombre, email, mensaje):
-    # Guarda los datos en Google Sheets
-    form_data = [nombre, email, mensaje]
-    sheet.append_row(form_data)
+# def save_to_google_sheets(nombre, email, mensaje):
+#     # Guarda los datos en Google Sheets
+#     form_data = [nombre, email, mensaje]
+#     sheet.append_row(form_data)
 
 # # Leer los datos de la hoja
 # data = sheet.get_all_records()
@@ -31,5 +30,31 @@ nombre = "Juan"
 email = "temp@gmail.com"
 mensaje = "Esto es una prueba"
 
+# Configura la conexión
+server = r'DESKTOP-44IC9KG\MSSQLSERVER_1'
+database = 'test'
+username = 'user_test'
+password = 'user_777'
+
+conn_str = (
+    f'DRIVER={{ODBC Driver 17 for SQL Server}};'
+    f'SERVER={server};'
+    f'DATABASE={database};'
+    f'UID={username};'
+    f'PWD={password}'
+)
+
+# Conexión
+conn = pyodbc.connect(conn_str)
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM tabla_test")
+
+row = cursor.fetchone()
+
+print(row)
+
+cursor.close()
+
 # Guardar los datos en Google Sheets
-save_to_google_sheets(nombre, email, mensaje)
+#save_to_google_sheets(nombre, email, mensaje)
+
